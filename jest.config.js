2133640eaf8121454cdd1436c38e924de84119f3
ffalt <ffalty@gmail.com>
2019-10-12 16:16:10 +0200

    fix(test): missing jest config

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+// https://github.com/thymikee/jest-preset-angular#brief-explanation-of-config
+const tsJestPreset = require('jest-preset-angular/jest-preset').globals['ts-jest'];
+
+module.exports = {
+	preset: 'jest-preset-angular',
+	roots: ['.'],
+	setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+	moduleNameMapper: {
+		"@app/(.*)": "<rootDir>/src/app/$1",
+		"@core/(.*)": "<rootDir>/src/app/modules/core/$1",
+		"@admin/(.*)": "<rootDir>/src/app/modules/admin/$1",
+		"@shared/(.*)": "<rootDir>/src/app/modules/shared/$1",
+		"@library/(.*)": "<rootDir>/src/app/modules/library/$1",
+		"@jam": "<rootDir>/src/app/modules/jam/index"
+	},
+	transformIgnorePatterns: ['node_modules/(?!(jest-test))', 'local/'],
+	globals: {
+		'ts-jest': {
+			...tsJestPreset,
+			tsConfig: 'tsconfig.spec.json'
+		}
+	}
+};
