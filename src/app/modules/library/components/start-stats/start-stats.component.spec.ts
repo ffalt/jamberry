0383b32f8258623a64882bc3adc42ff6ecfa4bf8
ffalt <ffalty@gmail.com>
2020-09-02 15:15:49 +0200

    test(library): split start components

--- src/app/modules/library/components/start-stats/start-stats.component.spec.ts
+++ src/app/modules/library/components/start-stats/start-stats.component.spec.ts
@@ -1,22 +1,21 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
-import {StartPageComponent, StartSectionComponent} from '@library/components';
+import {StartStatsComponent} from '@library/components';
 import {TEST_LIBRARY_IMPORTS, TEST_LIBRARY_PROVIDERS} from '@library/library.module.mock';
-import {MockComponent} from 'ng-mocks';
 
-describe('StartPageComponent', () => {
-	let component: StartPageComponent;
-	let fixture: ComponentFixture<StartPageComponent>;
+describe('StartStatsComponent', () => {
+	let component: StartStatsComponent;
+	let fixture: ComponentFixture<StartStatsComponent>;
 
 	beforeEach(async () =>
 		TestBed.configureTestingModule({
 			imports: [...TEST_LIBRARY_IMPORTS],
 			providers: [...TEST_LIBRARY_PROVIDERS],
-			declarations: [StartPageComponent, MockComponent(StartSectionComponent)]
+			declarations: [StartStatsComponent]
 		}).compileComponents()
 	);
 
 	beforeEach(() => {
-		fixture = TestBed.createComponent(StartPageComponent);
+		fixture = TestBed.createComponent(StartStatsComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
 	});
