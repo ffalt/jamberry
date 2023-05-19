f3021b46ab3492302caf84916a586cae28bf634a
ffalt <ffalty@gmail.com>
2023-05-19 20:07:03 +0200

    chore(types): fix parameter type

--- src/app/modules/user/pages/sessions-page/sessions-page.component.ts
+++ src/app/modules/user/pages/sessions-page/sessions-page.component.ts
@@ -13,8 +13,8 @@ export class SessionsPageComponent implements OnInit {
 	constructor(private jam: JamService, private auth: JamAuthService, private notify: NotifyService) {
 	}
 
-	trackByFn(index: number, value: Jam.UserSession): string {
-		return value.id;
+	trackByFn(index: number, value: { session: Jam.UserSession; isExpired: boolean; }): string {
+		return value.session.id;
 	}
 
 	ngOnInit(): void {
