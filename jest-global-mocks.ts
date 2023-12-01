6b6d2549e6ff7900dd608ea923d0545b6e1f1624
ffalt <ffalty@gmail.com>
2023-12-01 11:06:48 +0100

    chore(deps): upgrade to angular 17.0.5

--- jest-global-mocks.ts
+++ jest-global-mocks.ts
@@ -35,7 +35,3 @@ const mock = (): any => {
 Object.defineProperty(window, 'localStorage', {value: mock()});
 Object.defineProperty(window, 'sessionStorage', {value: mock()});
 Object.defineProperty(window, 'getComputedStyle', {value: () => ['-webkit-appearance']});
-
-Object.defineProperty(document.body.style, 'transform', {
-	value: () => ({enumerable: true, configurable: true})
-});
