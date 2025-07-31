aa7bf3359924476b95399353ee45181298bbdaf3
ffalt <ffalty@gmail.com>
2025-07-31 19:58:17 +0200

    chore(lint): fix

--- src/app/modules/jam/model/acousticbrainz-rest-data.ts
+++ src/app/modules/jam/model/acousticbrainz-rest-data.ts
@@ -8,30 +8,27 @@ export declare namespace AcousticBrainz {
 	}
 
 	export type KnownSections =
-		'danceability'
-		| 'gender'
-		| 'genre_dortmund'
-		| 'genre_electronic'
-		| 'genre_rosamerica'
-		| 'genre_tzanetakis'
-		| 'ismir04_rhythm'
-		| 'mood_acoustic'
-		|
-		'mood_aggressive'
-		| 'mood_electronic'
-		| 'mood_happy'
-		| 'mood_party'
-		| 'mood_relaxed'
-		| 'mood_sad'
-		| 'moods_mirex'
-		| 'timbre'
-		| 'tonal_atonal'
-		| 'voice_instrumental';
+		'danceability' |
+		'gender' |
+		'genre_dortmund' |
+		'genre_electronic' |
+		'genre_rosamerica' |
+		'genre_tzanetakis' |
+		'ismir04_rhythm' |
+		'mood_acoustic' |
+		'mood_aggressive' |
+		'mood_electronic' |
+		'mood_happy' |
+		'mood_party' |
+		'mood_relaxed' |
+		'mood_sad' |
+		'moods_mirex' |
+		'timbre' |
+		'tonal_atonal' |
+		'voice_instrumental';
 
 	export interface HighLevelSection {
-		all: {
-			[name: string]: number;
-		};
+		all: Record<string, number>;
 		probability: number;
 		value: string;
 		version: Version;
